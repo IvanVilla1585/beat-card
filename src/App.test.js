85324806d03792240cde4d7a3de2d84f6f9b4444
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2023-03-30 19:48:31 -0500

    feat: implement simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import calculateContribution from './calculateContribution'
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Beat Card/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('Should return at `Bon Appetit` text', () => {
+  const content = `4 1
+  3 10 2 9
+  7
+  `
+
+  const message = calculateContribution(content)
+
+  expect(message).toBe('Bon Appetit')
+})
+
+it('Should return at `5` text', () => {
+  const content = `4 1
+  3 10 2 9
+  12
+  `
+
+  const message = calculateContribution(content)
+
+  expect(message).toBe('5')
+})
