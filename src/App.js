07367b40de0ee9f856f8bd76f095d6d8a06fc0c0
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2023-03-30 19:47:57 -0500

    feat: implement the beat card view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from 'react'
+import { AppBar, Container, Grid, Typography, TextField, Button } from '@mui/material'
+
+import calculateContribution from './calculateContribution'
 
 function App() {
+  const [value, setValue] = useState('')
+  const [message, setMessage] = useState('')
+
+  const onChange = useCallback((event) => {
+    setValue(event.target.value)
+    const fileReader = new FileReader()
+
+    fileReader.onload = function (event) {
+      const content = event.target.result
+      const result = calculateContribution(content)
+
+      setMessage(result)
+    }
+
+   fileReader.readAsText(event.target.files[0])
+  }, [setValue, setMessage])
+
+  const onClick = (event) => {
+    event.preventDefault()
+    setValue('')
+    setMessage('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component='section'>
+      <AppBar position='static' sx={{ height: '50px' }}>
+        <Typography component='h1' align='center' sx={{ height: '50px', lineHeight: '50px' }}>
+          Beat Card
+        </Typography>
+      </AppBar>
+      <Grid container direction='column'>
+        <Grid container>
+          <Grid item sx={{ margin: '20px 0'}} xs={8}>
+              <TextField
+                type='file'
+                size="small"
+                value={value}
+                variant="outlined"
+                onChange={onChange}
+                inputProps={{ accept: '.txt' }}
+              />
+          </Grid>
+          <Grid item sx={{ margin: '20px 0'}} xs={4}>
+              <Button
+                onClick={onClick}
+                variant='contained'
+              >
+                Clear
+              </Button>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Typography variant='h5' align='center'>
+            {message}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
